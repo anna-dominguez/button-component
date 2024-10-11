9f573c713bbe3dacb3d9d7d315091beb4964166d
Vesta-nna <anna.dominguez.anna@gmail.com>
2024-10-11 18:12:43 +0200

    remove bg transparent

--- src/components/Blue.tsx
+++ src/components/Blue.tsx
@@ -2,31 +2,31 @@ const Blue = () => (
 	<section className="flex flex-wrap items-center gap-5 font-medium">
 		<button
 			type="button"
-			className="px-3.5 py-2.5 bg-indigo-700 hover:bg-indigo-800 outline-4 outline-indigo-100 focus:outline focus:outline-indigo-100 border border-transparent text-white h-fit shadow rounded text-sm focud:bg-indigo-800"
+			className="px-3.5 py-2.5 bg-indigo-700 hover:bg-indigo-800 outline-4 outline-indigo-100 focus:outline focus:outline-indigo-100 text-white h-fit shadow rounded text-sm focus:bg-indigo-800"
 		>
 			Button CTA
 		</button>
 		<button
 			type="button"
-			className="px-4 py-2.5 text-base flex gap-2 border border-transparent text-white focus:outline focus:outline-indigo-100 bg-indigo-700 hover:bg-indigo-800 outline-4 outline-indigo-100 rounded h-fit shadow focus:bg-indigo-800"
+			className="px-4 py-2.5 text-base flex gap-2 text-white focus:outline focus:outline-indigo-100 bg-indigo-700 hover:bg-indigo-800 outline-4 outline-indigo-100 rounded h-fit shadow focus:bg-indigo-800"
 		>
 			<i className="ri-star-line" /> <span>Button CTA</span>
 		</button>
 		<button
 			type="button"
-			className="flex gap-2 px-5 py-3 text-base text-white bg-indigo-700 border rounded shadow focus:outline focus:outline-indigo-100 border-trans outline-4 outline-indigo-100 arent hover:bg-indigo-800 h-fit focus:bg-indigo-800"
+			className="flex gap-2 px-5 py-3 text-base text-white bg-indigo-700 rounded shadow focus:outline focus:outline-indigo-100-trans outline-4 outline-indigo-100 arent hover:bg-indigo-800 h-fit focus:bg-indigo-800"
 		>
 			<span>Button CTA</span> <i className="ri-star-line" />
 		</button>
 		<button
 			type="button"
-			className="px-6 py-4 text-lg text-white bg-indigo-700 border rounded focus:outline focus:outline-indigo-100shadow border-trans outline-4 outline-indigo-100 arent hover:bg-indigo-800 h-fit focus:bg-indigo-800"
+			className="px-6 py-4 text-lg text-white bg-indigo-700 rounded shadow focus:outline focus:outline-indigo-100 outline-4 outline-indigo-100 hover:bg-indigo-800 h-fit focus:bg-indigo-800"
 		>
 			Button CTA
 		</button>
 		<button
 			type="button"
-			className="px-4 py-4 text-white bg-indigo-700 border rounded shadow focus:outline focus:outline-indigo-100 border-trans outline-4 outline-indigo-100 arent hover:bg-indigo-800 h-fit focus:bg-indigo-800"
+			className="px-4 py-4 text-white bg-indigo-700 rounded shadow focus:outline focus:outline-indigo-100 outline-4 outline-indigo-100 hover:bg-indigo-800 h-fit focus:bg-indigo-800"
 		>
 			<i className="ri-star-line" />
 		</button>
--- src/components/Red.tsx
+++ src/components/Red.tsx
@@ -2,31 +2,31 @@ const Red = () => (
 	<section className="flex flex-wrap items-center gap-5 font-medium">
 		<button
 			type="button"
-			className="px-3.5 py-2.5 bg-red-600  outline-4 outline-red-100 focus:outline focus:outline-red-100 border border-transparent text-white h-fit shadow rounded text-sm hover:bg-red-700 focus:bg-red-700"
+			className="px-3.5 py-2.5 bg-red-600  outline-4 outline-red-100 focus:outline focus:outline-red-100 text-white h-fit shadow rounded text-sm hover:bg-red-700 focus:bg-red-700"
 		>
 			Button CTA
 		</button>
 		<button
 			type="button"
-			className="px-4 py-2.5 text-base flex gap-2 border border-transparent focus:outline focus:outline-red-100 text-white bg-red-600  outline-4 outline-red-100 rounded h-fit shadow hover:bg-red-700 focus:bg-red-700"
+			className="px-4 py-2.5 text-base flex gap-2 focus:outline focus:outline-red-100 text-white bg-red-600  outline-4 outline-red-100 rounded h-fit shadow hover:bg-red-700 focus:bg-red-700"
 		>
 			<i className="ri-star-line" /> <span>Button CTA</span>
 		</button>
 		<button
 			type="button"
-			className="flex gap-2 px-5 py-3 text-base text-white bg-red-600 border border-transparent rounded shadow focus:outline focus:outline-red-100 outline-4 outline-red-100 h-fit hover:bg-red-700 focus:bg-red-700"
+			className="flex gap-2 px-5 py-3 text-base text-white bg-red-600 rounded shadow focus:outline focus:outline-red-100 outline-4 outline-red-100 h-fit hover:bg-red-700 focus:bg-red-700"
 		>
 			<span>Button CTA</span> <i className="ri-star-line" />
 		</button>
 		<button
 			type="button"
-			className="px-6 py-4 text-lg text-white bg-red-600 border border-transparent rounded shadow focus:outline focus:outline-red-100 outline-4 outline-red-100 h-fit hover:bg-red-700 focus:bg-red-700"
+			className="px-6 py-4 text-lg text-white bg-red-600 rounded shadow focus:outline focus:outline-red-100 outline-4 outline-red-100 h-fit hover:bg-red-700 focus:bg-red-700"
 		>
 			Button CTA
 		</button>
 		<button
 			type="button"
-			className="px-4 py-4 text-white bg-red-600 border border-transparent rounded shadow focus:outline focus:outline-red-100 outline-4 outline-red-100 h-fit hover:bg-red-700 focus:bg-red-700"
+			className="px-4 py-4 text-white bg-red-600 rounded shadow focus:outline focus:outline-red-100 outline-4 outline-red-100 h-fit hover:bg-red-700 focus:bg-red-700"
 		>
 			<i className="ri-star-line" />
 		</button>
